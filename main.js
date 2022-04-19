f8701c7f163be32d7b69eb84db92022e7624326c
Jen <jenniferlynnebarry@gmail.com>
2022-04-19 11:40:41 -0700

    added styling to project

--- main.js
+++ main.js
@@ -12,14 +12,7 @@ var planetsArray = [
     ['Sun', 27.9] 
 ];
 
-// 1. Populate the dropdown element with the data found in the planets array. 
-    // The value of each option should be the planet's name. 
-    // Use the following built-in methods: .forEachdocument.getElementById.appendChild` 
-    // iterate over the planets array with .forEach
-        // use the document.createElementeById to create an option
-        // then give that option a value 
-        // give that option text content/inner HTML
-        // we have to append that to the select box (select line 18)
+// Populate the dropdown element with the data found in the planets array. 
 
 const select = document.getElementById("planets");
 planetsArray.forEach((item) => {
@@ -30,37 +23,29 @@ planetsArray.forEach((item) => {
         
 })
 
-
 function calculateWeight(weight, planetName) { 
-  // 2. Write the code to return the correct weight 
-    // input: weight in pounds on earth
-    // output: weight on various plants compared to on earth 
-
     for (i = 0; i < planetsArray.length; i++) {
         if (planetsArray[i][0] == planetName) {
             return weight * planetsArray[i][1];
         }
     }
-
  }
 
 function handleClickEvent(e) {
-// 3. Create a variable called userWeight and assign the value of the user's weight. (.getElementById)
+
 let userWeight = document.getElementById("user-weight").value;
 
-// 4. Create a variable called planetName and assign the name of the selected planet from the drop down. (.getElementById (name of id).value)
 let planetName = document.getElementById("planets").value;
 console.log(userWeight, planetName);
-// 5. Create a variable called result and assign the value of the new calculated weight. 
+
 let result = calculateWeight(userWeight, planetName);
 
-// 6. Write code to display the message shown in the screenshot. ("Your weight is X/result on planet Y/what was chosen")
 let message = document.getElementById("output");
 
 message.innerHTML= `If you were on ${planetName}, you would weigh ${result}lbs!`;
 } 
    
-// 7. Set the #calculate-button element's onclick method to use the handleClickEvent function. (once I do 3, 4, 7- console.log)
+// Set the #calculate-button element's onclick method to use the handleClickEvent function. (once I do 3, 4, 7- console.log)
 document.getElementById("calculate-button").addEventListener("click", () => handleClickEvent())
 
 // 8. Make it look nice by attaching  a style.css file to your index.html and writing some basic styling, 
